ff11ea5931604dac64541e5530aac171af2606a7
JayaKrishnaNamburu <namburu1995@gmail.com>
2020-03-09 20:40:05 +0530

    Updates configs no luck :(

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const config = {
-  webpack(config, options) {
+  webpack(config) {
+    config.node = {
+      fs: "empty",
+      module: "empty"
+    };
+
     config.module.rules.unshift({
       test: /\.worker\.js/,
       use: {
@@ -13,6 +18,7 @@ const config = {
 
     config.output.globalObject = 'typeof self !== "object" ? self : this';
 
+    // Temporary fix for https://github.com/zeit/next.js/issues/8071
     config.plugins.forEach(plugin => {
       if (plugin.definitions && plugin.definitions["typeof window"]) {
         delete plugin.definitions["typeof window"];
